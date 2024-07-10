9c7f98cf4eb3fe36df7409dd92655593e5f4329a
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-07-11 00:00:29 +0300

    add indicator when no todos found

--- app/src/main/java/com/mostafadevo/todotrackercompose/ui/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/mostafadevo/todotrackercompose/ui/screens/homescreen/HomeScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -194,6 +196,28 @@ fun HomeScreen(
                         mViewModel.onEvent(HomeScreenUiEvent.onDismissTodoDetailesBottomSheet)
                     },
                     onEvent = mViewModel::onBottomSheetEvent
+                )
+            }
+        }
+        if (uiState.todos.isEmpty() && uiState.selectedSegmentIndex == 0) {
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No To-Do items found",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        } else if (uiState.todos.isEmpty() && uiState.selectedSegmentIndex == 1) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No Done items found",
+                    modifier = Modifier.padding(16.dp)
                 )
             }
         }
