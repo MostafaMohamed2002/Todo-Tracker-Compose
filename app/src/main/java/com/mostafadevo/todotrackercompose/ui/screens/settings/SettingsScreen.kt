4ebdd75dbda33f3ab77567c431247e66f20cf7eb
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-07-26 03:29:18 +0300

    ♻ refactor: replace LargeTopAppBar with TopAppBar in settings screen

--- app/src/main/java/com/mostafadevo/todotrackercompose/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/mostafadevo/todotrackercompose/ui/screens/settings/SettingsScreen.kt
@@ -9,11 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -38,7 +38,11 @@ fun SettingsScreen(
       .nestedScroll(scrollBehavior.nestedScrollConnection)
       .padding(bottom = paddingValues.calculateBottomPadding()),
     topBar = {
-      LargeTopAppBar(
+      TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+          containerColor = MaterialTheme.colorScheme.primary,
+          titleContentColor = MaterialTheme.colorScheme.onPrimary
+        ),
         title = { Text("Settings \uD83D\uDEE0\uFE0F") },
         scrollBehavior = scrollBehavior,
       )
